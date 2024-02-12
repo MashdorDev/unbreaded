1e6971daa7def43eac2aca153dd25f5863174a5e
rylerbleau <73083256+rylerbleau@users.noreply.github.com>
2024-02-12 17:16:09 -0500

    changed respawn gamemode to cpp

--- Source/unbread/Private/SRespawnGameMode.cpp
+++ Source/unbread/Private/SRespawnGameMode.cpp
@@ -3,3 +3,70 @@
 
 #include "SRespawnGameMode.h"
 
+#include "GameFramework/DefaultPawn.h"
+
+
+void ASRespawnGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	CurLives = MaxLives;
+
+	SetSpawnLocation(FindPlayerStart(UGameplayStatics::GetPlayerController(GetWorld(), 0))->GetTransform());
+	UGameplayStatics::GetPlayerPawn(GetWorld(),0)->OnDestroyed.AddDynamic(this, &ASRespawnGameMode::RespawnPlayer);
+	
+}
+
+void ASRespawnGameMode::SpawnPlayer()
+{
+	// converting to Fvector and Frotator
+	const FVector Pos = SpawnLocation.GetLocation();
+	const FRotator* Rot = new FRotator(SpawnLocation.GetRotation().Rotator());
+
+
+	// spawn actor and bind on destroy
+	UClass* PawnClass = DefaultPawnClass;
+	APawn* SpawnedPlayer = GetWorld()->SpawnActor<APawn>(PawnClass, SpawnLocation);
+	SpawnedPlayer->OnDestroyed.AddDynamic(this, &ASRespawnGameMode::RespawnPlayer);
+
+	// possess spawned player
+	if(APawn* SpawnedPawn = dynamic_cast<APawn*>(SpawnedPlayer))
+	{
+		UGameplayStatics::GetPlayerController(GetWorld(), 0)->Possess(SpawnedPawn);
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("Possessed player"));
+
+	}
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Cannot cast AActor to APawn"));
+	}
+}
+
+void ASRespawnGameMode::RespawnPlayer(AActor* Destroyed)
+{
+	CurLives--;
+	CheckLoss();
+	
+	// spawn player with delay, or no delay if 0.
+	if(SpawnDelay > 0.0f)
+	{
+		FTimerHandle UnusedHandle;
+        GetWorldTimerManager().SetTimer(
+        UnusedHandle, this, &ASRespawnGameMode::SpawnPlayer, SpawnDelay, false);
+	}
+	else SpawnPlayer();
+	
+}
+
+void ASRespawnGameMode::CheckLoss()
+{
+	if(CurLives <= 0)
+	{
+		UGameplayStatics::OpenLevel(GetWorld(), "MainMenu");
+	}
+}
+
+void ASRespawnGameMode::SetSpawnLocation(FTransform Location)
+{
+	SpawnLocation = Location;
+}
--- Source/unbread/Public/SRespawnGameMode.h
+++ Source/unbread/Public/SRespawnGameMode.h
@@ -4,14 +4,45 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameMode.h"
+#include "Kismet/GameplayStatics.h"
 #include "SRespawnGameMode.generated.h"
 
 /**
  * 
  */
+
+
 UCLASS()
+
 class UNBREAD_API ASRespawnGameMode : public AGameMode
 {
 	GENERATED_BODY()
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FTransform SpawnLocation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float SpawnDelay;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int CurLives;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	int MaxLives = 3;
+	
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void SpawnPlayer();
+
+	UFUNCTION()
+	void RespawnPlayer(AActor* Destroyed);
+
+	UFUNCTION()
+	void CheckLoss();
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void SetSpawnLocation(FTransform Location);
 	
 };
