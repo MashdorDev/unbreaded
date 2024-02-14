b94f37df136ae89ed22cbb4df41193253e7838ee
Dor <dorzairi@ymail.com>
2024-02-14 11:48:26 -0500

    create BTTask_Melee.H

---
+++ Source/unbread/Public/BTTask_Melee.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTTask_Fire.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class UNBREAD_API UBTTask_Melee : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+
+public:
+	UBTTask_Melee(const FObjectInitializer& ObjectInitializer);
+
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+	void Shoot(bool NewBool);
+
+	class ASRangedAICharacter* Chr = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Blackboard")
+	bool ShouldFire = true;
+};
